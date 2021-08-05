b2fe189ac93db3e89d86b9fed820340bce1c04e3
RodCerB <rodrigo.cbraga@hotmail.com>
2021-08-05 15:59:42 -0300

    finalizado componente contact

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,4 +1,5 @@
 import styled from "styled-components"
+import error from '../assets/icon-error.svg'
 
 const Contact = () =>{
 
@@ -6,6 +7,13 @@ const Contact = () =>{
         <section>
             <p>35,000+ ALREADY JOINED</p>
             <h2>Stay up-to-date with what we're doing</h2>
+            <form >
+                <div className='emailContainer'>
+                    <input type="email" className='formEmail' placeholder='Enter your email address'/>
+                    {/* <img src={error} alt="erro" className='formError'/> */}
+                </div>
+                <button className='btnRed'>Contact Us</button>
+            </form>
         </section>
     </Wrapper>
 }
@@ -16,12 +24,55 @@ const Wrapper = styled.main`
         background-color: var(--clr-blue);
         text-align: center;
         color: white;
-        padding-top: 4.5rem;
+        padding: 4.5rem 0 4.6875rem;
 
         h2{
             color: inherit;
             width: 28.75%;
             margin: 2.875rem auto 2.5625rem;
+        }
+        form{
+            display: flex;
+            justify-content: center;
+            gap: 1.125rem;
+
+            .emailContainer{
+                position: relative;
+                z-index: 9;
+                width: 21vw;
+                border-radius: 10px;
+                background-color: white;
+                padding: 1.125rem 1.375rem;
+                padding-right: 0.5rem;
+                display: flex;
+                justify-content: space-between;
+            }
+
+            .formEmail{
+                color: var(--clr-text);
+                font-family: 'Rubik', sans-serif;
+                font-size: 1.125rem;
+                border: none;
+                outline: none;
+            }
+        }
+    }
+
+    @media (max-width: 900px){
+        section{
+            h2{
+                width: 82%;
+            }
+            form{
+                flex-direction: row;
+                flex-wrap: wrap;
+                width: 82%;
+                margin: auto;
+
+                .emailContainer, .btnRed{
+                    width: 100%;
+                }
+            }
         }
     }
 
